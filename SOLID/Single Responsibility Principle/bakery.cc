844aded42cf3782b78c63b8cee8b076e524b701e
Raj <theraj1999@gmail.com>
2025-02-21 20:35:02 +0530

    Initial commit

---
+++ SOLID/Single Responsibility Principle/bakery.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+
+// Class for baking bread
+class BreadBaker {
+public:
+    void bakeBread(){
+        std::cout << "Baking high-quality bread..."<< std::endl;;
+    }
+};
+
+class InventoryManager {
+public:
+    void manageInventory() {
+        std::cout << "Managing inventory..." << std::endl;
+    }
+};
+
+class SupplyOrder {
+public:
+    void orderSuplies(){
+        std::cout << "Ordering supplies..." << std::endl;
+    }
+};
+
+class CustomerService {
+public:
+    void cleanBakery(){
+        std::cout << "Cleaning the bakery..." << std::endl;
+    }
+};
+
+int main(){
+    BreadBaker baker;
+    InventoryManager inventory_manager;
+    SupplyOrder supply_order;
+    CustomerService customer_service;
+    BreadBaker bread_baker;
+
+    baker.bakeBread();
+    inventory_manager.manageInventory();
+    supply_order.orderSuplies();
+    customer_service.cleanBakery();
+    bread_baker.bakeBread();
+}
